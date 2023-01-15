0b54a5e6af1075651590b7040c63f6e83d3bdc86
William Oliveira <williamdesouzaoliveira@gmail.com>
2023-01-15 11:57:50 -0300

    Entendendo sobre variaveis e contextos de execução

--- Aula03/aula3.js
+++ Aula03/aula3.js
@@ -29,34 +29,3 @@ console.log(`Imprime constante com número ${num}`)
 //Erro ao tentar atribuir outro valor a constante num
 // num = 11
 
-
-var sobrenome = "Oliveira"
-let empresa = "Tecnologia"
-if(true){
-
-    var nome = "William"
-    console.log(empresa)
-
-}
-
-function mostraVariavel(){
-
-   let nomeDentroDaFuncao = 'João'
-
-   if(true){
-
-    console.log(`Imprimindo o nome dentro da funcao e dentro do if: ${nomeDentroDaFuncao}`)
-    console.log(`Imprimindo o nome da empresa fora do contexto da funcao: ${empresa}`)
-   }
-}
-
-
-//variável não encontrada fora do escopo de onde foi definida
-//console.log(`Imprimindo o nome fora da funcao e dentro do if: ${nomeDentroDaFuncao}`)
-
-
-//Com a utilização de var é possível ter acesso a variável em qualquer escopo
-console.log(`${nome} e ${sobrenome}`)
-
-mostraVariavel()
-
