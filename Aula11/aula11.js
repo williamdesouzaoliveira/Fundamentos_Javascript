de21098426730ab3fa34542b118f45c896c2f2a6
William Oliveira <williamdesouzaoliveira@gmail.com>
2023-01-17 10:32:39 -0300

    Operador typeof

---
+++ Aula11/aula11.js
@@ -0,0 +1,20 @@
+
+
+let v1 = 10
+let v2 = "10"
+let v3 = v1===v2
+let v4 = {nome: "william"}
+
+console.log(`Valor da variável V1 : ${v1}`)
+console.log(`Valor da variável V3 : ${v2}`)
+console.log(`Valor da variável V3 : ${v3}`)
+console.log(`Valor da variável V4 : ${v4}`)
+
+
+// =======================utilizando typeof
+
+
+console.log(`Tipo da variável v1 utilizando typeof : ${typeof(v1)}`)
+console.log(`Tipo da variável v2 utilizando typeof : ${typeof(v2)}`)
+console.log(`Tipo da variável v3 utilizando typeof : ${typeof(v3)}`)
+console.log(`Tipo da variável v4 utilizando typeof : ${typeof(v4)}`)
