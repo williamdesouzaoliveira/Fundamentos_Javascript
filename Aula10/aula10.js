7a56771b3eda8d19dedb7e027071516e2e35e398
William Oliveira <williamdesouzaoliveira@gmail.com>
2023-01-17 10:08:59 -0300

    Operador ternário

---
+++ Aula10/aula10.js
@@ -0,0 +1,26 @@
+
+
+let num=10
+let num1 = 2
+let num2 = 20
+
+
+let res = num%2
+
+
+if(res==0){
+
+    console.log("Par")
+}else{
+    console.log("impar")
+}
+
+
+//Utilizando o operador ternário
+
+res = (num%2 ? "Par": "Impar")
+
+//console.log(typeof(num%2))
+
+let resultado = (num1>num2 ? "Verdadeiro" : "Falso")
+console.log(resultado)
