58a80e4d245ecc239d59bda5a28c755a6a90381e
William Oliveira <williamdesouzaoliveira@gmail.com>
2023-01-17 07:49:43 -0300

    Operadores Bitwise

---
+++ Aula08/aula8.js
@@ -0,0 +1,13 @@
+
+
+let n1=13
+let n2=14
+
+
+    console.log(`Operação Bitwise equivalente ao AND : ${n1&n2}`)
+    console.log(`Operação Bitwise equivalente ao OR : ${n1|n2}`)
+    console.log(`Operação Bitwise ou exclusivo, equivalente o XOR  : ${n1 ^ n2}`)
+
+    //multiplicando ou dividindo um valor bit a bi
+    console.log(`Deslocando um bit para a esquerda : ${n1 << 1}`)
+    console.log(`Deslocando um bit para a direita : ${n2>>1}`)
