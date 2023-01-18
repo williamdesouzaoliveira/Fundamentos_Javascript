105530fe0d632eeb7577f8083f5bdc1530d8d359
William Oliveira <williamdesouzaoliveira@gmail.com>
2023-01-18 09:45:12 -0300

    Condicionais IF/ELSE IF

---
+++ Aula13/aula13.js
@@ -0,0 +1,25 @@
+
+let num=5
+let energia=60
+let clima='sol'
+if(num > 10){
+
+    console.log("Numeral maior que 10")
+}else if(num >5){
+
+    console.log('Numeral está entre 6 e 10')
+}else{
+    console.log('Numeral menor ou igual a 5')
+}
+
+//==================================
+
+if(energia > 70 && clima == 'sol'){
+    console.log('vou a praia')
+}else{
+    console.log('vou ao cinema')
+}
+
+
+
+
