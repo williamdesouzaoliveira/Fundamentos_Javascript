3bd889e60034aea7b2c430b30d85475d715727b6
William Oliveira <williamdesouzaoliveira@gmail.com>
2023-01-18 10:32:31 -0300

    Estrutura Switch case

---
+++ Aula14/aula14.js
@@ -0,0 +1,22 @@
+
+let colocacao=7
+
+switch(colocacao){
+
+    case 1:
+        console.log('Primeiro lugar')
+        break
+    case 2:
+        console.log('segundo lugar')
+        break
+    case 3:
+        console.log('terceiro lugar')
+        break
+        default:
+            console.log('nenhuma colocação definida')
+            break
+
+        case 4: case 5: case 6:
+            console.log('premio de participação')
+            break
+}
