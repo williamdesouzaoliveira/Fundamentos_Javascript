8b2b411562380ee59f7915f8e6c602b60a07db30
William Oliveira <williamdesouzaoliveira@gmail.com>
2023-01-20 11:36:18 -0300

    Funções parametrizadas

---
+++ Aula22 - Funções com parâmetros/index.js
@@ -0,0 +1,28 @@
+// const PADRAO = 0
+// function soma(n1=PADRAO,n2=PADRAO){
+
+//     res  = n1* n2
+//     return res
+
+
+// }
+
+// let resultado  = soma(2,8)
+
+// console.log(resultado)
+let valor=0
+
+function add(v){
+
+    return valor+=v
+}
+
+add(11)
+console.log(valor)
+add(22)
+console.log(valor)
+add(50)
+console.log(valor)
+
+
+
