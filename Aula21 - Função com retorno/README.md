e28ac42029844a0f64bef2c946fb4b9f7a6121f2
William Oliveira <williamdesouzaoliveira@gmail.com>
2023-01-20 10:13:21 -0300

    Funções com retorno

---
+++ Aula21 - Função com retorno/README.md
@@ -0,0 +1,1 @@
+## Funções com retorno em javascript
---
+++ Aula21 - Função com retorno/index.js
@@ -0,0 +1,24 @@
+
+// function canal(){
+
+//     return 'william Oliveira'
+// }
+
+
+// let texto = canal()
+// console.log(texto)
+
+function calcula(){
+
+    let n1=20
+    let n2=30
+
+    let res = n1*n2
+    return res
+
+}
+
+calcula()
+
+let valor = calcula()
+console.log(`Total retornoado da função : ${valor}`)
