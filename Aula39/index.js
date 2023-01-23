1b48ab6461f6f601ba47b338a679a2ec7e5739b6
William Oliveira <williamdesouzaoliveira@gmail.com>
2023-01-23 09:26:34 -0300

    Entendendo a relação dos elementos no DOM em Javascript - Aula 2

---
+++ Aula39/index.js
@@ -0,0 +1,34 @@
+
+
+const cursos=document.querySelectorAll(".curso")
+const caixa1=document.querySelector("#caixa1")
+const c1_2=document.querySelector("#c1_2")
+
+//capitura o pai do elemento
+console.log(c1_2.parentNode.parentNode.parentNode.children[4])
+
+//lista os filhos do elemento / retorna os elementos da coleção
+// console.log(caixa1.children)
+
+//retorna true se o elemento possui filhos
+// console.log(caixa1.children)
+// console.log(caixa1.children.length>0 ? "Tem filhos" : "Não contem filhos")
+
+// console.log(caixa1.children.length>0 ?"sim":"Não")
+
+//altera o texto do primeiro elemento da div
+// console.log(caixa1.firstElementChild.innerHTML='William')
+
+//alterando o texto de algum elemento específico.
+// console.log(caixa1.children[1].innerHTML='teste')
+
+
+
+
+// console.log(caixa1.hasChildNodes > 0 ? "possui filhos" : "Não possui filhos" )
+
+
+
+
+
+
