1f1c307ac1969aced915640244fe787cc5ca980a
William Oliveira <williamdesouzaoliveira@gmail.com>
2023-02-08 16:49:32 -0300

    ITERADORES

---
+++ Aula51/index.js
@@ -0,0 +1,18 @@
+
+const valores = [2,3,10,11,14,8,4,17,19]
+const it = valores[Symbol.iterator]()
+
+console.log(it.next().value)
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+
+
